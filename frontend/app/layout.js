90a5b31c250ddef726ba1246df9804df3d8965c0
fromjyce <jaya2004kra@gmail.com>
2025-02-01 18:14:28 +0530

    init frontend

---
+++ frontend/app/layout.js
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "DevMate",
+  description: "AI Code Editor",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className="antialiased"
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
